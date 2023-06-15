3689b795e4253b1dcaafae33e60955bf9ee9dce5
Mohammedaoudi <meddaou2003@gmail.com>
2023-06-15 10:40:51 -0700

    tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * c program with printf prints with pr....
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
