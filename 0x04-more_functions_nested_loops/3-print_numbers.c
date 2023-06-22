ce2ec7ca9d7fa4c0bb8095d14bb021119aa846f9
Mohammedaoudi <meddaou2003@gmail.com>
2023-06-22 12:04:22 -0700

    tasks

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,9 +7,9 @@
 
 void print_numbers(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 48; c <= 48 + 9; c++)
 	{
 		_putchar(c);
 	}
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
- * more_numbers -  prints 10 times the numbers, from 0 to 14, ine.
- * Return : void
+ * more_numbers - print more numbers
  */
+
 void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0 ; j <= 14 ; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 				_putchar('1');
