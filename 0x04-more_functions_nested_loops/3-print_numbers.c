002d58b0e95d4af53db0450328e20bdddbd3ab6c
Mohammedaoudi <meddaou2003@gmail.com>
2023-06-22 11:17:07 -0700

    tasks

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers- prints the numbers, from 0 to 9, followed by a new line.
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i < '10'; i++)
+		_putchar(i);
+	_putchar('\n';
+}
+
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers- prints the numbers, from 0 to 9, followed by a new line
+ */
+
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i < '10'; i++)
+	{
+		if (i == 2 || i == 4)
+			;
+		else
+			_putchar(i);
+	}
+	_putchar('\n');
+}
+
