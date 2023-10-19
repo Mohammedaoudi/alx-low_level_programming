1f2d0401da93da954593f9e1ec93415fb192d866
Mohammedaoudi <meddaou2003@gmail.com>
2023-10-19 13:48:01 -0700

    easy

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,18 +9,16 @@
  */
 size_t print_list(const list_t *h)
 {
- size_t k = 0;
-
- while (h)
- {
- if (!h->str)
- printf("[0] (nulll)\n");
- else
- printf("[%u] %s\n", h->len, h->str);
- h = h->next;
- k++;
- }
-
- return (k);
+size_t k = 0;
+while (h)
+{
+if (!h->str)
+printf("[0] (nulll)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+k++;
+}
+return (k);
 }
 
